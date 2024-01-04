74888c5fc6e898233eed56b8ca60333c9a79258b
DEV-COM <kwp@tlen.pl>
2024-01-04 10:00:21 +0100

    10:00

---
+++ pingpong.py
@@ -0,0 +1,33 @@
+class A:
+    def ping(self):
+        print('ping', self)
+
+
+class B(A):
+    def pong(self):
+        print('pong', self)
+
+
+class C(A):
+    def pong(self):
+        print('PONG', self)
+
+
+class D(B, C):
+    def ping(self):
+        super().ping()
+        print('post-ping', self)
+
+    def pingpong(self):
+        self.ping()
+        super().ping()
+        self.pong()
+        super().pong()
+        C.pong(self)
+
+print(D.mro())
+d = D()
+d.pong()
+d.ping()
+C.pong(d)
+
