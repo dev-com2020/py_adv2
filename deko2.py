4e24eccc7a56805cfaa07ce211ffb882e243d4d6
DEV-COM <kwp@tlen.pl>
2024-01-04 10:33:12 +0100

    10:33

--- deko2.py
+++ deko2.py
@@ -7,6 +7,15 @@ def fun_decorator(some_func):
         print(some_func())
     return wrapper
 
+@fun_decorator
 def a_funct():
     text = "To jest oryginalny tekst"
     return text
+
+@fun_decorator
+def a_funct1():
+    text = "To jest oryginalny tekst drugiej funkcji"
+    return text
+
+a_funct()
+a_funct1()
