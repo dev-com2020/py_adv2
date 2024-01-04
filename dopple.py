bfe227dcc2cc41d84777ddb44a4a29006554aaf8
DEV-COM <kwp@tlen.pl>
2024-01-04 09:39:03 +0100

    09:38

--- dopple.py
+++ dopple.py
@@ -1,3 +1,24 @@
 class DoppleDict(dict):
-    pass
+    def __setitem__(self, key, value):
+        super().__setitem__(key, [value] * 2)
 
+
+# slownik['klucz'] = 3 # __setitem__
+
+dd = DoppleDict(pierwszy=1)
+dd['drugi'] = 2  # setitem
+dd.update(trzeci=3)
+print(dd)
+
+
+class AnswerDict(dict):
+    def __getitem__(self, item):
+        return 100
+
+ad = AnswerDict(a="tomek")
+print(ad)
+print(ad['a'])
+slownik = {}
+slownik.update(ad)
+print(slownik['a'])
+print(slownik)
