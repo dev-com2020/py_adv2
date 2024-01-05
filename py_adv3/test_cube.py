8cabdf5811d23a93a96eb81e7c67279d1f13b284
DEV-COM <kwp@tlen.pl>
2024-01-05 15:00:24 +0100

    piątek 15:00

--- py_adv3/test_cube.py
+++ py_adv3/test_cube.py
@@ -1,10 +1,41 @@
 import cube
 import unittest
-
+import pytest
 
 class TestCube(unittest.TestCase):
     def test_0(self):
         self.assertEqual(cube.cube(0), 0)
+
+    def test_1(self):
+        self.assertEqual(cube.cube(1), 1)
+
+    def test_2(self):
+        self.assertEqual(cube.cube(2), 8)
+
+    def test_3(self):
+        self.assertEqual(cube.cube(3), 27)
+
+    def test_4(self):
+        with self.assertRaises(TypeError):
+            cube.cube('x')
+
+class TestPyCube:
+    def test_0(self):
+        assert cube.cube(0) == 0
+
+    def test_1(self):
+        assert cube.cube(1) == 1
+
+    def test_2(self):
+        assert cube.cube(2) == 8
+
+    def test_3(self):
+        assert cube.cube(3) == 27
+
+    def test_4(self):
+        with pytest.raises(TypeError):
+            cube.cube('x')
+
 
 
 if __name__ == '__main__':
