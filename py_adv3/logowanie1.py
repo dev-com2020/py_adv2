03017bab9cb0ae71fb9f77227a8e50cad585981a
DEV-COM <kwp@tlen.pl>
2024-01-05 09:29:00 +0100

    piątek 09:28

--- py_adv3/logowanie1.py
+++ py_adv3/logowanie1.py
@@ -3,13 +3,22 @@ import logging.handlers
 logger = logging.getLogger("my_logger")
 logger.setLevel(logging.DEBUG)
 logging.basicConfig()
-logger.addHandler(
-    logging.handlers.TimedRotatingFileHandler(
+formatter = logging.Formatter(
+    fmt=(
+        "%(asctime)s | %(levelname)s | "
+        "%(name)s | %(filename)s:%(lineno)d | "
+        "%(message)s"
+    )
+)
+handler = logging.handlers.TimedRotatingFileHandler(
         filename='../app.log',
         when="D",
         backupCount=30,
+        encoding="UTF-8"
     )
-)
+
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 logger.log(logging.CRITICAL, "Komunikat o błędzie krytycznym")
 logger.error("Komunikat o błędzie")
