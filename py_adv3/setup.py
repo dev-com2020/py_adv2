e0ec8da89133e703f775f33ee017e12c2e551f03
DEV-COM <kwp@tlen.pl>
2024-01-05 16:03:54 +0100

    piątek 16:00

--- py_adv3/setup.py
+++ py_adv3/setup.py
@@ -6,3 +6,13 @@ setuptools.setup(
     version='1.0',
     ext_modules=[pyc],
 )
+
+packages = setuptools.find_packages(exclude=['test', 'docs'])
+entry_points = {
+    'distutils.command': [
+        'command_name = pakiet:Eggs',
+    ],
+    'console_scripts': [
+        'eggs = pakiet:Eggs'
+    ]
+}
